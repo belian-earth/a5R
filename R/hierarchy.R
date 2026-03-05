205283cc73346b3216233f10ce7ae7959dd58f6c
Hugh Graham <hugh.a.graham@gmail.com>
2026-03-05 11:38:17 +0000

    condense mutliple lines

--- R/hierarchy.R
+++ R/hierarchy.R
@@ -67,7 +67,6 @@ a5_cell_to_children <- function(cell, resolution = NULL) {
     check_resolution(resolution)
     vctrs::vec_assert(resolution, size = 1L)
   }
-  hex <- vctrs::vec_data(cell)
-  out <- a5_cell_to_children_rs(hex, resolution)
+  out <- a5_cell_to_children_rs(vctrs::vec_data(cell), resolution)
   new_a5_cell(out)
 }
